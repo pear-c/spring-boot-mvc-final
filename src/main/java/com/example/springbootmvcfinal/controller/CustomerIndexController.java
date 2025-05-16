f25299efbb35785a54e56e1543bb83b9ca2d637c
pear_c <zxcv9676@naver.com>
2025-05-16 13:32:31 +0900

    feat: 문의 글 분류(카테고리) 별 필터 기능 추가

--- src/main/java/com/example/springbootmvcfinal/controller/CustomerIndexController.java
+++ src/main/java/com/example/springbootmvcfinal/controller/CustomerIndexController.java
@@ -2,6 +2,7 @@ package com.example.springbootmvcfinal.controller;
 
 import com.example.springbootmvcfinal.domain.customer.Customer;
 import com.example.springbootmvcfinal.domain.inquiry.Inquiry;
+import com.example.springbootmvcfinal.domain.inquiry.InquiryCategory;
 import com.example.springbootmvcfinal.repository.InquiryRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -9,9 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 @Controller
@@ -22,18 +24,32 @@ public class CustomerIndexController {
     private final InquiryRepository inquiryRepository;
 
     @GetMapping
-    public String viewInquiryList(HttpSession session, Model model) {
+    public String viewInquiryList(@RequestParam(value ="category", required = false) InquiryCategory category,
+                                  HttpSession session, Model model) {
         Customer loginCustomer = (Customer) session.getAttribute("loginCustomer");
         if(loginCustomer == null) {
             return "redirect:/cs/login";
         }
 
         List<Inquiry> inquiryList = inquiryRepository.findByCustomerId(loginCustomer.getId());
-        Collections.sort(inquiryList, (o1, o2) -> {
-            return o2.getCreatedAt().compareTo(o1.getCreatedAt());
-        });
+        if(category == null) {
+            Collections.sort(inquiryList, (o1, o2) -> {
+                return o2.getCreatedAt().compareTo(o1.getCreatedAt());
+            });
+            model.addAttribute("inquiryList", inquiryList);
+        } else { // 분류 검색 시
+            List<Inquiry> filtered = new ArrayList<>();
+            for (Inquiry inquiry : inquiryList) {
+                if(inquiry.getCategory().equals(category)) {
+                    filtered.add(inquiry);
+                }
+            }
 
-        model.addAttribute("inquiryList", inquiryList);
+            Collections.sort(filtered, (o1, o2) -> {
+                return o2.getCreatedAt().compareTo(o1.getCreatedAt());
+            });
+            model.addAttribute("inquiryList", filtered);
+        }
 
         return "customerPage";
     }
