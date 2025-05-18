d94afd9af90a92b029c522af588cca1d91f3948c
pear_c <zxcv9676@naver.com>
2025-05-18 10:26:05 +0900

    feat: 답변 상세 페이지 이미지 출력, 새 창 띄우기 완료

--- src/main/java/com/example/springbootmvcfinal/controller/CsManagerAnswerController.java
+++ src/main/java/com/example/springbootmvcfinal/controller/CsManagerAnswerController.java
@@ -1,6 +1,8 @@
 package com.example.springbootmvcfinal.controller;
 
+import com.example.springbootmvcfinal.domain.attachment.Attachment;
 import com.example.springbootmvcfinal.domain.inquiry.Inquiry;
+import com.example.springbootmvcfinal.repository.AttachmentRepository;
 import com.example.springbootmvcfinal.repository.InquiryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,17 +11,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/cs/admin/answer")
 public class CsManagerAnswerController {
 
     private final InquiryRepository inquiryRepository;
+    private final AttachmentRepository attachmentRepository;
 
     @GetMapping
     public String viewAnswerForm(@RequestParam("id") Long inquiryId, Model model) {
         Inquiry inquiry = inquiryRepository.findById(inquiryId);
         model.addAttribute("inquiry", inquiry);
+
+        List<Attachment> attachments = attachmentRepository.findByInquiryId(inquiryId);
+        model.addAttribute("attachments", attachments);
+
         return "adminAnswerForm";
     }
 }
